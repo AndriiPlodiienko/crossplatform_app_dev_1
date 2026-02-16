76c3b74cf093885157b0a62a2a17973143997c94
AndriiPlodiienko <rantugaming@gmail.com>
2026-02-16 23:30:30 +0200

    Add MCV logic app about planets

--- main.py
+++ main.py
@@ -1,2 +1,47 @@
 import tkinter as tk
 
+class ModelApp:
+    def __init__(self):
+        self.planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+        self.counter = 0
+    
+    def step_up(self):
+        self.counter += 1
+        if self.counter >= len(self.planets):
+            self.counter = 0
+    
+    def get_values(self):
+        return self.planets[self.counter]
+    
+class ViewApp(tk.Frame):
+    def __init__(self, master, on_click):
+        super().__init__(master)
+        self.pack()
+
+        self.label = tk.Label(self, bg="white", text="")
+        self.label.pack()
+
+        button = tk.Button(self, text="PRESS", command=on_click)
+        button.pack()
+
+    def update_label(self, text):
+        self.label.config(text=text)
+
+class ControllerApp:
+    def __init__(self, root):
+        self.model = ModelApp()
+        self.view = ViewApp(root, self.on_button_click)
+        self.update_view()
+
+    def on_click(self):
+        self.model.step_up()
+        self.update_view()
+
+    def update_view(self):
+        value = self.model.get_values()
+        self.view.update_label(value)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = ControllerApp(root)
+    root.mainloop()
